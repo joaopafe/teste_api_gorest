e2897ba5a01abab8470611174511a0b3eb08b785
João Paulo <joao.ea2020@gmail.com>
2024-01-24 15:38:59 -0300

    refac: implementacao do cenario 5

--- tests/scenarios/1_products/scenario-5.test.ts
+++ tests/scenarios/1_products/scenario-5.test.ts
@@ -1,7 +1,49 @@
+import { ProductsClient } from "../../clients/products/productsClient";
+import { config } from "../../config";
+import { createProduct } from "../../utils/createProduct";
+
+const productsClient = new ProductsClient();
+
 describe("Cenário 5 - Remoção de produtos (DELETE /api/v1/products/{id})", () => {
-    test("5.1 - Remover produto por ID através de requisição válida", () => {});
+  const titleValid = config.postProductsValid.title;
+  const priceValid = config.postProductsValid.price;
+  const descriptionValid = config.postProductsValid.description;
+  const categoryIdValid = config.postProductsValid.categoryId;
+  const imagesValid = config.postProductsValid.images;
 
-    test("5.2 - Retornar erro para parâmetro 'id' do tipo string não numérica", () => {});
+  let idCreated = 0;
 
-    test("5.3 - Retornar erro para parâmetro 'id' não cadastrado", () => {});
+  beforeAll(async () => {
+    idCreated = await createProduct({
+      title: titleValid,
+      price: priceValid,
+      description: descriptionValid,
+      categoryId: categoryIdValid,
+      images: imagesValid,
+    });
+  });
+
+  test("5.1 - Remover produto por ID através de requisição válida", async () => {
+    const response = await productsClient.deleteProduct({
+      id: idCreated,
+    });
+
+    expect(response.status).toEqual(200);
+  });
+
+  test("5.2 - Retornar erro para parâmetro 'id' do tipo string não numérica", async () => {
+    const response = await productsClient.deleteProduct({
+      id: "idInvalid",
+    });
+
+    expect(response.status).toEqual(400);
+  });
+
+  test("5.3 - Retornar erro para parâmetro 'id' não cadastrado", async () => {
+    const response = await productsClient.deleteProduct({
+      id: idCreated,
+    });
+
+    expect(response.status).toEqual(404);
+  });
 });
