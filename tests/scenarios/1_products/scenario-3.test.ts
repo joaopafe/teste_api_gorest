9f2772e946cf86a1f063dc7ca425d8a9fb1ae8ca
João Paulo <joao.ea2020@gmail.com>
2024-01-24 16:44:12 -0300

    refac: atualizacao dos metodos usados no cenario 3

--- tests/scenarios/1_products/scenario-3.test.ts
+++ tests/scenarios/1_products/scenario-3.test.ts
@@ -26,7 +26,7 @@ describe("Cenário 3 - Obtenção de produtos por ID (GET /api/v1/products/{id})
   });
 
   test("3.1 - Listar produto por ID através de requisição válida", async () => {
-    const response = await productsClient.listProductsByIdValid({
+    const response = await productsClient.listProductsById({
       id: idFetched,
     });
 
@@ -40,7 +40,7 @@ describe("Cenário 3 - Obtenção de produtos por ID (GET /api/v1/products/{id})
   });
 
   test("3.2 - Retornar erro de formato para parâmetro 'id' do tipo string não numérica", async () => {
-    const response = await productsClient.listProductsByIdInvalid({
+    const response = await productsClient.listProductsById({
       id: "a",
     });
 
@@ -55,7 +55,7 @@ describe("Cenário 3 - Obtenção de produtos por ID (GET /api/v1/products/{id})
   });
 
   test("3.3 - Retornar erro para parâmetro 'id' não cadastrado", async () => {
-    const response = await productsClient.listProductsByIdInvalid({
+    const response = await productsClient.listProductsById({
       id: idNonExistent,
     });
 
