bf8120d74a895a1e86f4d62bc4b5ebb5c9cfa5bd
João Paulo <joao.ea2020@gmail.com>
2024-01-24 14:22:48 -0300

    refac: atualizacao do cenario 1

--- tests/scenarios/1_products/scenario-1.test.ts
+++ tests/scenarios/1_products/scenario-1.test.ts
@@ -2,25 +2,24 @@ import { ProductsClient } from "../../clients/products/productsClient";
 import { config } from "../../config";
 import { productsSchema, responseErrorSchema } from "../../schemas";
 
+const productsClient = new ProductsClient();
+
 describe("Cenário 1 - Obtenção de produtos (GET /api/v1/products)", () => {
-  const productsClient = new ProductsClient();
+  const limitValid = config.getProductsValid.limit;
+  const offsetValid = config.getProductsValid.offset;
 
   test("1.1 - Listar produtos através de requisição válida", async () => {
-    const limit = config.getProductsValid.limit;
-    const offset = config.getProductsValid.offset;
-
     const response = await productsClient.listProducts({
-      limit,
-      offset,
+      limit: limitValid,
+      offset: offsetValid,
     });
 
     expect(response.status).toEqual(200);
 
     const data = await response.json();
-
     await productsSchema.validate(data);
 
-    expect(data.length).toEqual(limit);
+    expect(data.length).toEqual(limitValid);
   });
 
   test("1.2 - Retornar erro de formato para falta de parâmetros obrigatórios", async () => {
@@ -29,7 +28,6 @@ describe("Cenário 1 - Obtenção de produtos (GET /api/v1/products)", () => {
     expect(response.status).toEqual(400);
 
     const data = await response.json();
-
     await responseErrorSchema.validate(data);
 
     expect(data.statusCode).toEqual(response.status);
@@ -37,23 +35,22 @@ describe("Cenário 1 - Obtenção de produtos (GET /api/v1/products)", () => {
   });
 
   test("1.3 - Retornar lista vazia para requisição com parâmetros 'offset' com valor acima do último ID cadastrado", async () => {
-    const limit = 100000;
+    const highLimit = 100000;
 
     const preRequestResponse = await productsClient.listProducts({
-      limit,
+      limit: highLimit,
       offset: 1,
     });
 
     expect(preRequestResponse.status).toEqual(200);
 
     const preData = await preRequestResponse.json();
-
     await productsSchema.validate(preData);
 
     const higgerId = preData[preData.length - 1].id;
 
     const response = await productsClient.listProducts({
-      limit,
+      limit: highLimit,
       offset: higgerId + 1,
     });
 
@@ -65,12 +62,11 @@ describe("Cenário 1 - Obtenção de produtos (GET /api/v1/products)", () => {
   });
 
   test("1.4 - Retornar erro de formato para requisição com parâmetro 'limit' do tipo string", async () => {
-    const limit = config.getProductsInvalid.limit;
-    const offset = config.getProductsValid.offset;
+    const limitInvalid = config.getProductsInvalid.limit;
 
     const response = await productsClient.listProductsInvalid({
-      limit,
-      offset,
+      limit: limitInvalid,
+      offset: offsetValid,
     });
 
     expect(response.status).toEqual(400);
@@ -84,18 +80,16 @@ describe("Cenário 1 - Obtenção de produtos (GET /api/v1/products)", () => {
   });
 
   test("1.5 - Retornar erro de formato para requisição com parâmetro 'offset' do tipo string", async () => {
-    const limit = config.getProductsValid.limit;
-    const offset = config.getProductsInvalid.offset;
+    const offsetInvalid = config.getProductsInvalid.offset;
 
     const response = await productsClient.listProductsInvalid({
-      limit,
-      offset,
+      limit: limitValid,
+      offset: offsetInvalid,
     });
 
     expect(response.status).toEqual(400);
 
     const data = await response.json();
-
     await responseErrorSchema.validate(data);
 
     expect(data.statusCode).toEqual(response.status);
@@ -103,12 +97,12 @@ describe("Cenário 1 - Obtenção de produtos (GET /api/v1/products)", () => {
   });
 
   test("1.6 - Retornar erro de formato para requisição com parâmetro 'limit' e 'offset' do tipo string", async () => {
-    const limit = config.getProductsInvalid.limit;
-    const offset = config.getProductsInvalid.offset;
+    const limitInvalid = config.getProductsInvalid.limit;
+    const offsetInvalid = config.getProductsInvalid.offset;
 
     const response = await productsClient.listProductsInvalid({
-      limit,
-      offset,
+      limit: limitInvalid,
+      offset: offsetInvalid,
     });
 
     expect(response.status).toEqual(400);
