92118cbe4d80961081ae4ba8091f8d09787bb977
João Paulo <joao.ea2020@gmail.com>
2024-01-27 16:43:10 -0300

    implementacao do cenario 10

--- tests/scenarios/2_users/scenario-10.test.ts
+++ tests/scenarios/2_users/scenario-10.test.ts
@@ -1,7 +1,43 @@
+import { UsersClient } from "../../clients/users/usersClient";
+import { config } from "../../config";
+import { createUser } from "../../utils/createUser";
+
+const usersClient = new UsersClient();
+
 describe("Cenário 10 - Remoção de usuários (DELETE /api/v1/users/{id})", () => {
-    test("5.1 - Remover usuário por ID através de requisição válida", () => {});
+  let idCreated = 0;
 
-    test("5.2 - Retornar erro para parâmetro 'id' do tipo string não numérica", () => {});
+  beforeAll(async () => {
+    idCreated = await createUser({
+      email: config.postUsersValid.email,
+      name: config.postUsersValid.name,
+      password: config.postUsersValid.password,
+      role: config.postUsersValid.role,
+      avatar: config.postUsersValid.avatar,
+    });
+  });
 
-    test("5.3 - Retornar erro para parâmetro 'id' não cadastrado", () => {});
+  test("5.1 - Remover usuário por ID através de requisição válida", async () => {
+    const response = await usersClient.deleteUser({
+      id: idCreated,
+    });
+
+    expect(response.status).toEqual(200);
+  });
+
+  test("5.2 - Retornar erro para parâmetro 'id' do tipo string não numérica", async () => {
+    const response = await usersClient.deleteUser({
+      id: "abc",
+    });
+
+    expect(response.status).toEqual(404);
+  });
+
+  test("5.3 - Retornar erro para parâmetro 'id' não cadastrado", async () => {
+    const response = await usersClient.deleteUser({
+      id: idCreated,
+    });
+
+    expect(response.status).toEqual(404);
+  });
 });
